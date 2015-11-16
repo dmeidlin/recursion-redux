0e343d111a2cc8046837bb7838b6ca8a3f5a1452
dmeidlin <git_hub@nym.hush.com>
2015-11-16 17:55:02 -0500

    Write the getElementByClassName function without using an inner function that produces side effects.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function (className, node) {
+    // your code here
+    'use strict';
+    // set node to a default value if no node is supplied.
+    node = node || document.body;
+  
+    var nodes = [],
+        childResults = [],
+        parts = node.className.split(' '),
+        i;
+    //Check if the class name is used in the current node
+    if (parts.indexOf(className) >= 0) {
+      //if it is, push the element to the nodes array
+        nodes.push(node);
+    }
+    //Iterate over any child nodes.
+    for (i = 0; i < node.children.length; i++) {
+        childResults = getElementsByClassName(className, node.children[i]);
+        nodes = nodes.concat(childResults);
+      }
+  
+    return nodes;
 };
